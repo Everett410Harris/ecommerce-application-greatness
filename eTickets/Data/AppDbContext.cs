1e9dd54326200249b2c3137a8c38ace057096ba0
Everett Harris <79everettharris@gmail.com>
2023-01-22 16:39:37 -0500

    sql configured

--- eTickets/Data/AppDbContext.cs
+++ eTickets/Data/AppDbContext.cs
@@ -8,7 +8,7 @@ using DbContext = Microsoft.EntityFrameworkCore.DbContext;
 
 namespace eTickets.Data
 {
-    public class AppDbContext:DbContext
+    public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
